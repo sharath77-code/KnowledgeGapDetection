e8f7f5fbcee1fe315a9e40f5ee40f4601be7b824
Sharath B <sharathabsg7@gmail.com>
2026-08-07 23:47:49 +0530

    Clean commit

--- app.py
+++ app.py
@@ -728,6 +728,23 @@ def load_resources():
 def home():
     return render_template("index.html")
 
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/analytics")
+@login_required
+def analytics():
+    return result()
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/privacy")
+def privacy():
+    return render_template("privacy.html")
+
 @app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
